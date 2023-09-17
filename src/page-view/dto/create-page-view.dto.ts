55a45a7a1a06f49d4e5b3afb4d8385156fa04966
Diego Vanelli <diego.vanelli@dbrsa.com.br>
2023-09-17 20:17:03 -0300

    add class validator

--- src/page-view/dto/create-page-view.dto.ts
+++ src/page-view/dto/create-page-view.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePageViewDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreatePageViewDto {
+  @IsNotEmpty({ message: 'O page não pode ser vazio' })
+  page: string;
+
+  @IsNotEmpty({ message: 'O user não pode ser vazio' })
+  user: string;
+
+  @IsNotEmpty({ message: 'O date não pode ser vazio' })
+  date: string;
+}
